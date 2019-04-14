63bccbdff57268c3c3eec6ef8bf2fe748b9f8c8f
duck_lu <33782897+252843759@users.noreply.github.com>
2019-04-14 22:30:09 +0800

    Create 283_MoveZeros.c

---
+++ array/283_MoveZeros.c
@@ -0,0 +1,12 @@
+void moveZeroes(int* nums, int numsSize) {
+    int i, j;
+    
+    for (i = 0, j = 0; i < numsSize; ++i)
+    {
+        if (nums[i] != 0)
+            nums[j++] = nums[i];
+    }
+    
+    for (; j < numsSize; ++j)
+        nums[j] = 0;
+}
