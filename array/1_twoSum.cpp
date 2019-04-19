536cd55548b66b33f1317c5de961efe4f1008fed
duck_lu <33782897+252843759@users.noreply.github.com>
2019-04-19 09:47:35 +0800

    add python is more easy to understand

--- array/1_twoSum.cpp
+++ array/1_twoSum.cpp
@@ -1,3 +1,20 @@
+// python code
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for index, num in enumerate(nums):
+            anothor_num = target-num
+            if anothor_num in d.keys():
+                return [d[anothor_num], index]
+            else:
+                d[num] = index
+            
+        return []
+            
+
+// cpp
+// return {}; indicates "return an object of the function's return type initialized with an empty list-initializer". 
+// The exact behaviour depends on the returned object's type.                   
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
